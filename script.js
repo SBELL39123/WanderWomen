970349980d9ebc69676719ddc113693484998f49
SydneyBell <sbell39@depaul.edu>
2025-04-10 11:33:04 -0500

    Responsiveness, Netlify backend

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const status = document.getElementById('form-status');
+  const formData = new FormData(form);
+
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      status.innerHTML = '<p style="color: black;">Thank you for your message!</p>';
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = '<p style="color: red;">Oops! Something went wrong. Please try again later.</p>';
+    });
+}
+
+
+
+
+
+
+
+//initially used node.js to test forms locally 
+//backend is now on netlify.com through their form info 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
